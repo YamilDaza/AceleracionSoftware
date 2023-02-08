636109ec5d37b6c5ec849d5a468359a227a16689
Yamil Daza <devyamil1998@gmail.com>
2023-02-07 21:55:09 -0300

    Search_react

--- JavaScript/Search/search.js
+++ JavaScript/Search/search.js
@@ -1,6 +1,9 @@
 //Search
 document.addEventListener('keyup', e => {
    
+   /* if(e.key === 'Escape')e.target.value = ""; */
+
+
    if(e.target.matches('#search')){
       document.querySelectorAll('.colour').forEach(item => {
          item.textContent.toLowerCase().includes(e.target.value.toLowerCase()) ? item.classList.remove('filtro') : item.classList.add('filtro')
