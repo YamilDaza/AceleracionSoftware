00d143aea1dfbae7aedc57b2b9a7d80dbab7ab10
Yamil Daza <devyamil1998@gmail.com>
2023-02-15 17:11:54 -0300

    search two

--- JavaScript/Search/search.js
+++ JavaScript/Search/search.js
@@ -3,6 +3,7 @@ document.addEventListener('keyup', e => {
    
    /* if(e.key === 'Escape')e.target.value = ""; */
 
+   // console.log(e.target.value);
 
    if(e.target.matches('#search')){
       document.querySelectorAll('.colour').forEach(item => {
